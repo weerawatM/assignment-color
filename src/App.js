40a8f951b93345dbd3d59846c9b22aa1c8538f14
weerawatM <rattanachai.wrw@gmail.com>
2022-01-18 22:34:13 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import styled from 'styled-components'
 function App() {
+  // red>green>brown>blue
+  const defaultColor =["red","green" ,"brown" ,"blue"]
+  const colors = {
+    "red" :"green",
+    "green":"brown",
+    "brown": "blue",
+    "blue":"red"
+  }
+  const checkWin = (your,bot) =>colors[your] === bot ? "You Win" : colors[bot] === your ? "You lose" : "Draw"
+  const [color,setColor] =useState("")
+  const [botResult,setbotResult] =useState("")
+  const selectColor = (value) =>{setColor(value)}
+  useEffect(()=>{
+    if(!color)return;
+    setbotResult(Math.floor(Math.random()*4))
+    
+   },[color])
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Assignment</h1>
+      <SContainer >{defaultColor.map((e)=>{return <SCard style={{'backgroundColor':`${e}`}} onClick={()=>selectColor(e)}>{e}</SCard>})}</SContainer>
+      
+      <div >bot is {defaultColor[botResult]}</div>
+
+      <div>{checkWin(color,defaultColor[botResult])}</div>
+
     </div>
+    
   );
 }
 
 export default App;
+const SCard = styled.div`
+width:100px;
+height:200px;
+display:flex;
+justify-content:center;
+align-items:center;
+border-radius:20px;
+color:white;
+`
+
+const SContainer = styled.div`
+display:flex`
